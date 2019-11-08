781a9c3a1f109dfd2ffd1f58b52b98a91bc40aa4
Thomas Donovan <tdonovan79@gmail.com>
2019-11-08 05:09:10 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -68,7 +68,7 @@ def apply_clearance(cart)
   cart_index = 0
   while cart_index < cart.length do
     if cart[cart_index][:clearance]
-      new_item = (0.8 * cart[cart_index][:price])
+      new_item = (0.8 * cart[cart_index][:price]).round(2)
       cart[cart_index][:price] = new_item
       cart[cart_index][:price].round(2)
     end
